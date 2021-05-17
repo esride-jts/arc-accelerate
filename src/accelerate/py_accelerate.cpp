8882f342e637279374c4473df003b3ca2d3e5be9
Jan Tschada <jts@esri-de.com>
2021-05-17 15:31:02 +0200

    Easy to go pybind11 module

---
+++ src/accelerate/py_accelerate.cpp
@@ -0,0 +1,24 @@
+//   This file is part of arc-accelerate and enables the development of Geoprocessing Tools using C++.
+//   Copyright (C) 2021 Esri Deutschland GmbH
+//   Contact: Jan Tschada (j.tschada@esri.de)
+//   
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Lesser General Public License for more details.
+//   
+//   You should have received a copy of the GNU Lesser General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "py_accelerate.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(accelerate, m) {
+    m.doc() = "Accelerates the development of Geoprocessing tools using C++.";
+}
