2894d4821699ee9d32d3746e3f76030c0c2167fd
Jan Tschada <jts@esri-de.com>
2021-05-18 17:24:20 +0200

    Write need to specify fields and create feature class or table

--- test/test_accelerate.py
+++ test/test_accelerate.py
@@ -8,11 +8,23 @@ class TestAccelerateDataset(unittest.TestCase):
     def test_read(self):
         in_table = '{0}/ACLED_Sample'.format(os.environ['accelerate_sample_gdb'])
         dataset = Dataset()
-        dataset.read(in_table, ['*'], '1=1')
+        dataset.read(in_table, ['OID@', 'event_type'], '1=1')
         dataframe = dataset.to_pandas()
         row_count = dataframe.count()[0]
         self.assertLess(0, row_count, 'The dataframe should not be empty!')
+        head = dataframe.head()
+        self.assertIsNotNone(head, 'The first rows must not be none!')
+        columns = dataframe.columns
+        self.assertIsNotNone(columns, 'The columns must not be none!')
+        self.assertEqual(2, len(columns), 'Two columns were expected!')
 
+    def test_write(self):
+        in_table = '{0}/ACLED_Sample'.format(os.environ['accelerate_sample_gdb'])
+        dataset = Dataset()
+        dataset.read(in_table, ['OID@', 'event_type'], '1=1')
+        
+        out_table = '{0}/ACLED_Empty'.format(os.environ['accelerate_sample_gdb'])
+        dataset.write(out_table, ['OID@', 'event_type'])
 
 
 if __name__ == '__main__':
